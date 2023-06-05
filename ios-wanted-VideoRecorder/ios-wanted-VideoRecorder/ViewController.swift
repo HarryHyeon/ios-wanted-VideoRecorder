dd138e0005d40416031a2d56a3ba34c8234fc032
HarryHyeon <hharry0728@gmail.com>
2023-06-05 20:00:05 +0900

    프로젝트 초기화

---
+++ ios-wanted-VideoRecorder/ios-wanted-VideoRecorder/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ios-wanted-VideoRecorder
+//
+//  Created by Harry on 2023/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
